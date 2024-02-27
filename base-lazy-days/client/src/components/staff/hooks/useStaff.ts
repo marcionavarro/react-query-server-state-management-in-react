215547b61d769d4a6cf65df9324ce449e265f42f
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-02-27 01:13:51 -0300

    Code Quiz! Custom Hook for useStaff

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
+import { useQuery } from "react-query"; 
 
 import type { Staff } from "@shared/types";
 
@@ -7,18 +8,23 @@ import { filterByTreatment } from "../utils";
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
 
-// query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+interface UseStaff{
+  staff: Staff[];
+  filter: string;
+  setFilter: Dispatch<SetStateAction<string>>;
+}
 
-export function useStaff() {
+// query function for useQuery
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
+
+export function useStaff(): UseStaff {
   // for filtering staff by treatment
   const [filter, setFilter] = useState("all");
 
-  // TODO: get data from server via useQuery
-  const staff: Staff[] = [];
-
+ const fallback: Staff[] = [];
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff)
   return { staff, filter, setFilter };
 }
